f9a3c58ceeb619fca87e307ad01e82df8b2979cb
TOM LUCA <100872614+TOMLUCA7@users.noreply.github.com>
2023-08-19 12:25:38 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <main className="relative">
+      Nan component
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    </main>
+  );
+};
+
+export default App;
